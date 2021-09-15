d4e05c188048fc2ad8316cf92a002886b369be2b
UmanshSarabhai <49119948+UmanshSarabhai@users.noreply.github.com>
2021-09-16 00:17:23 +0530

    Add files via upload

---
+++ Application2/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Application2';
+  getValues(val:any){
+    console.warn(val);
+  }
+
+
+
+ 
+}
